7f40d8b50cbb6794b7eabf7c921e430f3e5526d2
coli-geonwoo <kkwoo001021@naver.com>
2023-12-13 16:29:06 +0900

    feat : 상품 객체 생성

---
+++ src/main/java/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    String productName;
+    int price;
+    int amount;
+    Product(String productName, int price, int amount){
+        this.productName=productName;
+        this.price=price;
+        this.amount=amount;
+    }
+
+
+}
